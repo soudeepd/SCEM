aa797b745f3d10061aab6ba01b29ef0faf66a1ac
soudeepd <33814114+soudeepd@users.noreply.github.com>
2018-01-12 16:35:01 -0600

    Update Usage-Example.R

--- Usage-Example.R
+++ Usage-Example.R
@@ -2,24 +2,22 @@
 # Preamble
 #-------------------------------------------------------------
 
-# Set the working directory 
+# Set the working directory and source all the functions given in the repository
 
 # Install the github package
-# devtools::install_github("soudeepd/SCEM") # not yet exported as a package
+# devtools::install_github("soudeepd/SCEM") # not yet exported as a package 
 
 #-------------------------------------------------------------
-# Read all the data files
+# Read the data file
 #-------------------------------------------------------------
-oxy <- read.csv("OxygenData3.csv")
-names(oxy)[3] = "ID"
-sheep <- subset(oxy,species=="Ovis")
-armenia = split(sheep,f = sheep$ID)
+oxy <- read.csv("armenia-data.csv")
+armenia = split(oxy,f = oxy$ID)
 
 
 #-------------------------------------------------------------
 # Implement the SCEM and Cosine method on the Armenia data
 #-------------------------------------------------------------
-results = SCEM(armenia,optimumbw = -0.33)
+results = SCEM(armenia,bandwidth = -0.33)
 cosine = makeFits(armenia)  
 rownames(cosine) = 1:nrow(cosine)
 
@@ -33,9 +31,21 @@ rrr.a = results$results[order(results$results$Cluster),]
 c.armenia = results$results$Cosine
 s.armenia = results$results$Season
 
+
+# ---------------------------------------------------------
+# Prepare relevant plots for the above analysis
+# ---------------------------------------------------------
+
+# Plotting the estimates of birth seasonality from the two methods
+plot(c.armenia,s.armenia,xlim = c(0,1),ylim = c(0,1),pch = 19)
+abline(0,1)
+legend("topleft","Tsaghkahovit")
+title(xlab = "estimated birth seasonality from cosine method",ylab = "estimated birth seasonality from SCEM",
+      outer = TRUE, line = 2)
+
 # Plotting the graphs of the clusters for Armenia data
-gnum = max(results$results$Cluster)
 dev.off()
+gnum = max(results$results$Cluster)
 par(mar=c(1.5,1.5,1,1), mfrow=c(2,2),oma = c(4, 4, 1, 1))
 for(i in 1:gnum){
   if(length(results$groups[[i]])>1){
@@ -50,19 +60,6 @@ for(i in 1:gnum){
   }
 }
 title(xlab = "distance from CEJ (mm)",ylab = expression(delta^18*O[VPDB]*' (per mil)'),outer = TRUE, line = 2)
-
-
-# ---------------------------------------------------------
-# Plot the birth seasonality estimates for the two methods 
-# ---------------------------------------------------------
-
-dev.off()
-par(mar=c(1.5,1.5,1,1), oma = c(4, 4, 1, 1))
-plot(c.armenia,s.armenia,xlim = c(0,1),ylim = c(0,1),pch = 19)
-abline(0,1)
-legend("topleft","Tsaghkahovit")
-title(xlab = "estimated birth seasonality from cosine method",ylab = "estimated birth seasonality from SCEM",
-      outer = TRUE, line = 2)
 
 
 # ---------------------------------------------------------
@@ -87,18 +84,15 @@ for (i in 1:length(amp)){
     cosfit.R[i,j,] = dd$Pearson
   }
 }
-mm = numeric(length(armenia))
-vv = mm
+birth.means = numeric(length(armenia))
+birth.var = numeric(length(armenia))
 for (i in 1:length(armenia)){
-  mm[i] = mean(c(cosfit.birth[,,i]))
-  vv[i] = var(c(cosfit.birth[,,i]))
+  birth.means[i] = mean(c(cosfit.birth[,,i]))
+  birth.var[i] = var(c(cosfit.birth[,,i]))
 }
 
 # Plots of the model fits to show the sensitivity to initial conditions
 dev.off()
-par(mfrow = c(2,2),mar = c(4,4,1,1))
-plot(mm,sqrt(vv),xlab = "mean of birth season",ylab = "standard deviation",pch = 19)
-hist(c(cosfit.period),25,freq = F,xlab = "estimated period",main = "")
-hist(c(cosfit.delay),25,freq = F,xlab = "estimated delay",main = "")
+par(mfrow = c(1,2),mar = c(4,4,1,1))
+plot(birth.means,sqrt(birth.var),xlab = "mean of birth season",ylab = "standard deviation",pch = 19)
 hist(c(cosfit.R^2),25,freq = F,xlab = "R-squared value",main = "")
-
